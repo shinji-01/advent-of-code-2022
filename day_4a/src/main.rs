2a1767e90605954d499e7ce5be27f7738fb69ed9
ano <ano>
2023-07-21 19:34:30 +0200

    day 4 part 1

---
+++ day_4a/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let m = include_str!("input.txt").lines().filter(|line| {
+        let mut pair = line.split(',');
+        let (section1, section2) = (pair.next().unwrap(), pair.next().unwrap());
+        let (mut section1_range, mut section2_range) = (section1.split('-'), section2.split('-'));
+        let begin_s1: usize = section1_range.next().unwrap().parse().unwrap();
+        let end_s1: usize = section1_range.next().unwrap().parse().unwrap();
+        let begin_s2: usize = section2_range.next().unwrap().parse().unwrap();
+        let end_s2: usize = section2_range.next().unwrap().parse().unwrap();
+
+        if (begin_s1 <= begin_s2 && end_s1 >= end_s2) || (begin_s2 <= begin_s1 && end_s2 >= end_s1) {
+            true
+        } else {
+            false
+        }
+    }).collect::<Vec<_>>();
+
+    println!("{:?}", m.len());
+}
